9d2f9191c568b29759261d01491daecaf557fbe5
Issac <issac.chawdhury@gmail.com>
2018-01-29 19:14:59 -0600

    Added a class and some stuff to game.py

---
+++ game.py
@@ -0,0 +1,22 @@
+class Player:
+	
+	def __init__(self, name,age,foods):
+		self.name = name
+		self.age = age
+		self.foods = foods
+
+	def birth_year():
+		return 2015 - age
+
+people = [Person("Ed",11,["hotdogs","jawbreakers"]),Person("Edd",11,["broccoli"]),Person("Eddy",12,["chunky puffs","jawbreakers"])]
+
+sum = 0
+for person in people
+	sum = sum+person.age
+
+print("The average age is: " +str(sum/len(people)))
+
+
+
+
+
