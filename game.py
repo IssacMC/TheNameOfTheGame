d6c9c6318e19bf1ec65848e60044343f8b397111
Issac <issac.chawdhury@gmail.com>
2018-01-29 19:57:42 -0600

    Added some more weapon classes, as well as a main sorta thing

--- game.py
+++ game.py
@@ -1,20 +1,53 @@
-class Player:
-	
+class Weapon:
+	def __str__(self):
+		return self.name
+
+class Person:
 	def __init__(self, name,age,foods):
 		self.name = name
 		self.age = age
 		self.foods = foods
-
+	def __str__(self):
+		return "Name: " +self.name\
+			+" Age: " + str(self.age)\
+			+" Favorite food: " +str(self.foods[0])
 	def birth_year():
 		return 2015 - age
 
-people = [Person("Ed",11,["hotdogs","jawbreakers"]),Person("Edd",11,["broccoli"]),Person("Eddy",12,["chunky puffs","jawbreakers"])]
+class Rock(Weapon):
+	def __init__(self):
+		self.name = "Rock"
+		self.desc = "A rock for hitting things"
+		self.damage = 5
 
-sum = 0
-for person in people
-	sum = sum+person.age
+class Dagger(Weapon):
+	def __init__(self):
+		self.name = "Dagger"
+		self.desc = "A dagger for stabbing things"
+		self.damage = 10
 
-print("The average age is: " +str(sum/len(people)))
+class RustySword(Weapon):
+	def __init__(self):
+		self.name = "Rusty sword"
+		self.desc = "A sword for slashing things"
+		self.damage = 20
+
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+try:
+	print("You were born in {}.".format(2015-int(age)))
+except ValueError:
+	print("Unable to calculate the year you were born, "\
+		+ '"{}" is not a number.'.format(age))
+
+
+	
+		
+
+
+
+
+
 
 
 
